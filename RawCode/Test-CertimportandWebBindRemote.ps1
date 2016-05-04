81ea48c49bafd852ef74036e285df0bb0ce7c87b
jeffbuenting <jeffbuenting@gmail.com>
2016-05-04 13:11:40 -0400

    updated module manifest
    updated to version 1.6
    added test cert import and bindings code to raw code

---
+++ RawCode/Test-CertimportandWebBindRemote.ps1
@@ -0,0 +1,28 @@
+﻿
+
+
+import-module F:\GitHub\Certificates\certificates.psm1 -Force
+
+$File = Get-Item -Path $CertPFX
+Copy-item -Path $CertPFX -Destination "\\$CRMServer\c$\temp\$($File.Name)" -Force
+
+$Certificate = invoke-command -ComputerName $CRMServer -ArgumentList "c:\temp\$($File.Name)",$Password -ScriptBlock {
+    param ( 
+        [String]$CertPath,
+        [System.Security.SecureString]$Password
+    )
+
+    if ( -Not ( Test-Path -Path $CertPath ) ) { Throw "Certificate path does not exist on remote computer" }
+
+    $Cert = Import-pfxCertificate -FilePath $CertPath -CertStoreLocation cert:\localmachine\My -Password $Password 
+
+    Write-Output $Cert
+}
+
+
+#$Certificate | FL *
+
+Import-CertWebSite -ComputerName $CRMServer -WebSiteName "Microsoft Dynamics CRM" -Certificate $Certificate -verbose
+
+
+
