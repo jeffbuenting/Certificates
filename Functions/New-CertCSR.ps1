3c45c5d27b68c415b303675ba2244bf8c3826dfe
unknown <administrator@KINGS-WOOD.LOCAL>
2021-01-28 20:08:45 -0500

    New-CertCSR - support for OpenSSL

--- Functions/New-CertCSR.ps1
+++ Functions/New-CertCSR.ps1
@@ -11,6 +11,7 @@
         https://github.com/chrisdee/Scripts/blob/master/PowerShell/Working/certificates/GenerateCertificateSigningRequest(CSR).ps1
         https://dille.name/blog/2016/11/08/using-a-microsoft-ca-to-secure-docker/
         https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/certreq_1#syntax
+        https://dille.name/blog/2011/12/23/how-to-request-a-certificate/
 
 #>
 
@@ -41,10 +42,7 @@
         [Parameter ( ParameterSetName = 'INF',Mandatory = $True  ) ]
         [Parameter ( ParameterSetName = 'FromParameters',Mandatory = $True  )]
         [Parameter ( ParameterSetName = 'OpenSSL',Mandatory = $True )]
-        [String]$CSRFile,
-
-        [Parameter ( ParameterSetName = 'OpenSSL',Mandatory = $True )]
-        [Switch]$OpenSSL
+        [String]$CSRFile
 
     )
 
@@ -126,7 +124,46 @@ $SAN
         }
 
         'OpenSSL' {
-            
+            # ----- This section relys on third party openssl.exe 
+            if ( Get-Command openssl.exe -ErrorAction SilentlyContinue ) {
+                Write-Verbose "OpenSSL.exe is installed and in the windows path"
+                $OSSLPath = ''
+            }
+            Elseif ( Test-Path -Path 'C:\Program Files\OpenSSL-Win64\bin\openssl.exe' ) {
+                # ----- Try default path
+                Write-Verbose "OpenSSL.exe is installed at C:\Program Files\OpenSSL-Win64\bin\"
+                $OSSLPath = 'C:\Program Files\OpenSSL-Win64\bin\'
+            }
+            Else {
+                Throw "New-CertCSR : openssl is either not installed or cannot be found"
+            }
+
+            # ----- Generate Config file for OpenSSL
+            # https://www.switch.ch/pki/manage/request/csr-openssl/
+            $CSRCNF = @()
+            $CSRCNF += "[ req ]"
+            $CSRCNF += "default_bits = 2048"
+            $CSRCNF += "default_md = sha256"
+            $CSRCNF += "prompt = no"
+            $CSRCNF += "encrypt_key = no"
+            $CSRCNF += "distinguished_name = dn"
+            $CSRCNF += "req_extensions = req_ext"
+            $CSRCNF += " "
+            $CSRCNF += "[ dn ]"
+            $CSRCNF += "C = CH"
+            $CSRCNF += "O = $ORGNAME"
+            $CSRCNF += "CN = $FQDN"
+            $CSRCNF += " "
+            $CSRCNF += "[ req_ext ]"
+            $CSRCNF += "subjectAltName = DNS:$FQDN, DNS:$($SubjectAlternativeNames -join ', DNS:')"
+
+            $CSRCNF | Out-file ($CSRFile | Split-Path -Parent)\CSR.cnf
+
+            # ----- Need to creat a key
+            & "$($OpenSSL)openssl.exe" genrsa -out $CSRFile.Replace('csr','key') 4096
+
+            # ----- Generate CSR
+            & "$($OpenSSL)openssl.exe" req -subj $FQDN -sha256 -new -key $CSRFile.Replace('csr','key') -config ($CSRFile | Split-Path -Parent)\CSR.cnf -out $CSRFile
         }
     }
 }
